17500e8ba89d93d26f954b27b2fae722f2af18ad
Siddharth Natrajan <sidddharthvn2014@gmail.com>
2017-11-15 17:39:08 -0600

    Some Files

---
+++ utils.js
@@ -0,0 +1,36 @@
+if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = (window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+    return window.setTimeout(callback, 1000/60);
+    });
+    }
+var utils = {};
+
+utils.captureMouse = function(element)
+{
+    var mouse = {x:0,y:0};
+    element.addEventListener('mousemove',function(event)
+    {
+        var x,y;
+        if(event.pageX || event.pageY)
+        {
+            x = event.pageX;
+            y = event.pageY;
+        }
+        else
+        {
+            x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+        x-= element.offsetLeft;
+        y-= element.offsetTop;
+
+        mouse.x = x;
+        mouse.y = y;
+    },false);
+
+    return mouse;
+};
